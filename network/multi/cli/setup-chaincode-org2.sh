0e74cee4b96ff3de726d8ddc7cbc4e402e82571d
simon <net0310@gmail.com>
2018-09-13 17:46:26 +0900

    인증서 도메인 변경.

--- network/multi/cli/setup-chaincode-org2.sh
+++ network/multi/cli/setup-chaincode-org2.sh
@@ -2,22 +2,22 @@
 
 echo 'Install ChainCode'
 
-CHAINCODE_DIR=github.com/hyperledger/fabric/examples/chaincode/go/
-CORE_PEER_ADDRESS=peer0.org2.kt.com:7051 peer chaincode install -n $1 -v $2 -p $CHAINCODE_DIR/$1_cc
+CHAINCODE_DIR=github.com/hyperledger/fabric/examples/chaincode/go/src
+CORE_PEER_ADDRESS=peer0.org2.example.com:7051 peer chaincode install -n marbles -v 0.1 -p $CHAINCODE_DIR/marbles
 
-CHAINCODE_DIR=github.com/hyperledger/fabric/examples/chaincode/go/
-CORE_PEER_ADDRESS=peer1.org2.kt.com:7051 peer chaincode install -n $1 -v $2 -p $CHAINCODE_DIR/$1_cc
+CHAINCODE_DIR=github.com/hyperledger/fabric/examples/chaincode/go/src
+CORE_PEER_ADDRESS=peer1.org2.example.com:7051 peer chaincode install -n marbles -v 0.1 -p $CHAINCODE_DIR/marbles
 
 echo 'Done'
 
 echo 'Instantiate ChainCode'
 
-#CORE_PEER_ADDRESS=peer0.org2.kt.com:7051 peer chaincode instantiate -o orderer0.kt.com:7050 \
-#--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/kt.com/orderers/orderer0.kt.com/msp/tlscacerts/tlsca.kt.com-cert.pem \
+#CORE_PEER_ADDRESS=peer0.org2.example.com:7051 peer chaincode instantiate -o orderer0.example.com:7050 \
+#--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
 #-C imchannel -n im -v 0.1 -c '{"Args":["init","a","200","b","200"]}' -P "OR   ('Org1MSP.peer','Org2MSP.peer')"
 #
-#CORE_PEER_ADDRESS=peer1.org2.kt.com:7051 peer chaincode instantiate -o orderer0.kt.com:7050 \
-#--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/kt.com/orderers/orderer0.kt.com/msp/tlscacerts/tlsca.kt.com-cert.pem \
+#CORE_PEER_ADDRESS=peer1.org2.example.com:7051 peer chaincode instantiate -o orderer0.example.com:7050 \
+#--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem \
 #-C imchannel -n im -v 0.1 -c '{"Args":["init","a","200","b","200"]}' -P "OR   ('Org1MSP.peer','Org2MSP.peer')"
 
 echo 'Done'
--- network/multi/cli/setup-channel-org2.sh
+++ network/multi/cli/setup-channel-org2.sh
@@ -2,14 +2,14 @@
 
 echo 'Join Channel..'
 
-CORE_PEER_ADDRESS=peer0.org2.kt.com:7051 peer channel join -b imchannel.block
-CORE_PEER_ADDRESS=peer1.org2.kt.com:7051 peer channel join -b imchannel.block
+CORE_PEER_ADDRESS=peer0.org2.example.com:7051 peer channel join -b mychannel.block
+CORE_PEER_ADDRESS=peer1.org2.example.com:7051 peer channel join -b mychannel.block
 
 echo 'Done'
 
 echo 'Update Org2 Anchor Peer..'
 
-peer channel update -o orderer0.kt.com:7050 -c imchannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
---tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/kt.com/orderers/orderer0.kt.com/msp/tlscacerts/tlsca.kt.com-cert.pem
+peer channel update -o orderer0.example.com:7050 -c mychannel -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx  \
+--tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer0.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 echo 'Done'
